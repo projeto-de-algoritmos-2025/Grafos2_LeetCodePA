986ffb450b9d896ddd9a0f15c0b06758eceba700
AlexandreLJr <ubc183@gmail.com>
2025-05-12 10:11:20 -0300

    comentando o codigo

--- questoes/1584.py
+++ questoes/1584.py
@@ -6,26 +6,26 @@ import heapq
 
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        def manhattan_distance(p1, p2):
+        def manhattan_distance(p1, p2): #função auxiliar para calcular a distância de Manhattan entre p1 e p2.
             return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
-
         n = len(points)
         visited = set()
-        min_heap = [(0, 0)]  
+        min_heap = [(0, 0)]  #Inicia a fila com indice 0 e custo 0
         total_cost = 0
 
-        while len(visited) < n:
+        while len(visited) < n: #contiua enquanto nem todos os nós forem conectados
             cost, current = heapq.heappop(min_heap)
 
-            if current in visited:
+            if current in visited: #ignora o nó caso já  tenha sido conectado
                 continue
 
-            total_cost += cost
-            visited.add(current)
+            total_cost += cost 
+            visited.add(current) #marca como já visitado
 
-            for next_point in range(n):
-                if next_point not in visited:
-                    dist = manhattan_distance(points[current], points[next_point])
+            for next_point in range(n): #percorre todos os nós para verificar quais não estão conectados
+                if next_point not in visited: #caso não foi contectado, prossegue
+                    dist = manhattan_distance(points[current], points[next_point]) #calcula a distancia entre nó atual e o proximo
                     heapq.heappush(min_heap, (dist, next_point))
 
         return total_cost
+    
